86227c5b5f1c12680c8c46c0406b4cd4873fa3cb
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-17 21:14:06 +0100

    Using a for did the job for two numbers and was natural to me instead to assigning result of the split in two different variables (maybe i know this Kata by heart ?)

--- src/test/java/com/fedou/katas/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/fedou/katas/stringcalculator/StringCalculatorTest.java
@@ -14,11 +14,16 @@ public class StringCalculatorTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 1234})
     void single_numbers_should_return_themselves(int input) {
-        Assertions.assertEquals(input, new StringCalculator().add(""+input));
+        Assertions.assertEquals(input, new StringCalculator().add("" + input));
     }
 
     @Test
     void two_numbers_separated_by_comma_should_be_added() {
         Assertions.assertEquals(3, new StringCalculator().add("1,2"));
+    }
+
+    @Test
+    void several_numbers_separated_by_comma_should_be_added() {
+        Assertions.assertEquals(6, new StringCalculator().add("1,2,3"));
     }
 }
