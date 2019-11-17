5ecce6a3c8a4d56e5c0a84748e17342434423aa9
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-17 21:30:53 +0100

    Assigning new values to arguments is a bad practice as mutating arguments in general. (Ok String is not mutable per se, but keep good habit will hurt less)

--- src/main/java/com/fedou/katas/stringcalculator/StringCalculator.java
+++ src/main/java/com/fedou/katas/stringcalculator/StringCalculator.java
@@ -5,20 +5,23 @@ public class StringCalculator {
     public static final String CUSTOM_DELIMITER_PREFIX = "//";
     private String delimiter = ",|\n";
 
-    public int add(String numbers) {
+    public int add(final String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
+        String numbersToParse;
         if (numbers.startsWith(CUSTOM_DELIMITER_PREFIX)) {
             int customDelimiterEnding = numbers.indexOf('\n');
             String customDelimiter = numbers.substring(
                     CUSTOM_DELIMITER_PREFIX.length(),
                     customDelimiterEnding);
             delimiter=customDelimiter;
-            numbers = numbers.substring(customDelimiterEnding+1);
+            numbersToParse = numbers.substring(customDelimiterEnding+1);
+        } else {
+            numbersToParse = numbers;
         }
         int result = 0;
-        for (String number : numbers.split(delimiter)) {
+        for (String number : numbersToParse.split(delimiter)) {
             result += Integer.parseInt(number);
         }
         return result;
