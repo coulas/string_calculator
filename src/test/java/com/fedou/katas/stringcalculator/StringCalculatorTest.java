356075944902ed8c63053cf8c979f608b2f25fc8
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-17 21:04:54 +0100

    a second test to return numbers when they are alone.

--- src/test/java/com/fedou/katas/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/fedou/katas/stringcalculator/StringCalculatorTest.java
@@ -2,10 +2,18 @@ package com.fedou.katas.stringcalculator;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringCalculatorTest {
     @Test
     void empty_string_count_as_0() {
         Assertions.assertEquals(0, new StringCalculator().add(""));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 1234})
+    void single_numbers_should_return_themselves(int input) {
+        Assertions.assertEquals(input, new StringCalculator().add(""+input));
     }
 }
