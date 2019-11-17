ebd76315fe23036ac5e7f338994108c35c8fb3a3
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-17 21:07:24 +0100

    every thing seems under control

--- src/main/java/com/fedou/katas/stringcalculator/StringCalculator.java
+++ src/main/java/com/fedou/katas/stringcalculator/StringCalculator.java
@@ -2,6 +2,9 @@ package com.fedou.katas.stringcalculator;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(numbers);
     }
 }
