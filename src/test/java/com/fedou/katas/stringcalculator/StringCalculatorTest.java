f060a0fcda9cc7a6f22279368d10c6cb4c999de7
Nicolas Fédou <nicolas.fedou@gmail.com>
2019-11-24 00:06:42 +0100

    Starting point

---
+++ src/test/java/com/fedou/katas/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,11 @@
+package com.fedou.katas.stringcalculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+    @Test
+    void truth() {
+        Assertions.assertTrue(false);
+    }
+}
